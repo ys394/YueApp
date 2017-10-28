d508ae18fa44c759805fcb9f0d591cf80ce5efe7
ys394 <songyue.cr@gmail.com>
2017-10-28 01:40:45 +0000

    Set root to auth page

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,7 @@ class AuthController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def new
+    render :text => "AUTH PAGE"
   end
 
   def login
