d8024e9065711f4d0a704fc9ceb98e8e9033b06b
ys394 <songyue.cr@gmail.com>
2017-10-28 01:19:48 +0000

    Initialize repo

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,13 @@
+class AuthController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  def new
+  end
+
+  def login
+  end
+  
+  def register
+  end
+end
