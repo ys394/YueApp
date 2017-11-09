d7cc21ece0084088d06ad09170185aa48141cd35
ys394 <songyue.cr@gmail.com>
2017-11-09 01:21:46 +0000

    Home page, list of overview

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,8 +2,9 @@ class AuthController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout "application"
   def new
-    render :text => "AUTH PAGE"
+    render "home"
   end
 
   def login
